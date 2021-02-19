3e1544d6baf9ba40370a486250aa33f32234e704
Abdullah0930 <Abdullahissack@outlook.com>
2021-02-18 21:30:57 -0600

    message

--- data/Jobs_project.sql
+++ data/Jobs_project.sql
@@ -1,2 +1,8 @@
 Select count (*) from data_analyst_jobs;
-
+-- 1. How many rows are in the data_analyst_jobs table? There are 1793 Rows in data_analyst_jobs
+SELECT company from data_analyst_jobs
+LIMIT 10;
+--2. What company is associated with the job posting on the 10th row? ExxonMObil 
+Select count (*) from data_analyst_jobs
+WHERE location = 'TN' or Location = 'KY';
+--How many postings are in Tennessee and/or in both? there are 21 in TN and 27 in Both.
